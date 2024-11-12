5d5a8df44a6304dfd4a7b60ac64e0dafea238ddf
MounikaEsramoni <mounikaesramoni918@gmail.com>
2024-11-12 12:15:46 +0530

    Add files via upload
    Add new changes

---
+++ Locators/LoginPage.cs
@@ -0,0 +1,10 @@
+﻿using OpenQA.Selenium;
+public class Login_Page
+{
+
+    public By Username = By.Id("user-name");
+
+    public By Password = By.Id("password");
+
+    public By Login = By.Id("login-button");
+}
